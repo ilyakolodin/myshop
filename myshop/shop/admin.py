469231279379614b469a65dc8f729baeacdfe602
ilyakolodin <il.kolodin2015@yandex.ru>
2024-04-15 21:21:09 +0300

    shop

--- myshop/shop/admin.py
+++ myshop/shop/admin.py
@@ -7,10 +7,10 @@ class ProductAdmin(admin.ModelAdmin):
     prtpopulated_fields = {'slug': ('name',)}
     
     
-@amin.register(Product)
+@admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug', 'price', 
-                    'available', 'created', 'updated']
-    list_filter = ['available', 'created', 'updated']
-    list_editable = ['price', 'available']
+                    'avaliable', 'created', 'updated']
+    list_filter = ['avaliable', 'created', 'updated']
+    list_editable = ['price', 'avaliable']
     prepopulated_fields = {'slug': ('name',)}
